1f41953f7420c1e8ab75b11a7fa0bb24d773b7c1
Yago Neves <yagolife1@gmail.com>
2022-09-14 13:38:42 -0300

    Project Created

---
+++ src/test/java/com/web/services/WebServicesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
